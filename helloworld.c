2edd9f8898c411c022d57b23fc2f7ada881a5554
vilassn <vilassninawe@gmail.com>
2022-09-18 00:27:42 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,28 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+	long a;
+	long x;
+	long y;
+
+	//printf("Enter value of x ");
+	//scanf("%ld", &x);
+	
+	//printf("Enter value of y ");
+	//scanf("%ld", &y);
+	
+	//a = x + y;
+	//printf("Multiplication of x and y is %ld\n", a);
+	
+	int val=0;
+	for (int i=0; i <= 100; i++)
+	{
+	 val = val + i;
+	}
+	
+	printf("value of val: %d\n", val);
+
+	return 0;
+}
